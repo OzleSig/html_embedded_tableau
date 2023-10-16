a2a6c22e2342434eecca8f9274e0947a41d844f4
Ozlem Sigbeku <ozlemsigbeku@protonmail.com>
2023-10-16 20:03:04 +0100

    initial commit

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ console.log("Hello World!");
 
 let viz;
 const exportpdfbutton = document.getElementById("exportPDF");
-const exportpptbutton =  document.getElementById("exportppt");
+const exportpptbutton = document.getElementById("exportppt");
 const filterbutton = document.getElementById("filterbutton")
-const viz_loc = document.getElementById("vizContainer"); 
+const viz_loc = document.getElementById("vizContainer");
 const url = "https://public.tableau.com/views/WOW2023_Week26/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
-const  options = {
-    device: "desktop", 
+const options = {
+    device: "desktop",
     height: "800px",
     width: "1000px",
 };
@@ -18,11 +18,11 @@ function initViz() {
 
 document.addEventListener("DOMContentLoaded", initViz);
 
-function exportPDFfunc(){
+function exportPDFfunc() {
     viz.showExportPDFDialog();
 }
 
-function exportpptfunc(){
+function exportpptfunc() {
     viz.showExportPowerPointDialog();
 }
 
@@ -37,10 +37,9 @@ function getrangevalue() {
     console.log(sheets);
     const sheetToFilter = sheets[0];
     console.log(sheetToFilter);
-    sheetToFilter.applyRangeFilterAsync("[Ice Extent vs Normal [MSM]]]",{min:minvalue, max:maxvalue}).then(alert("Filtered!"));
+    sheetToFilter.applyRangeFilterAsync("[Ice Extent vs Normal [MSM]]]", { min: minvalue, max: maxvalue }).then(alert("Filtered!"));
 }
 
 exportpdfbutton.addEventListener("click", exportPDFfunc)
 exportpptbutton.addEventListener("click", exportpptfunc)
 filterbutton.addEventListener("click", getrangevalue)
-
